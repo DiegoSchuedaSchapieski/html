d2669b3c08efbc97f3b5dbf9a8af8ba068e62b04
unknown <schuedaschhapieski@gmail.com>
2026-02-11 11:39:54 -0300

    fim da aula

--- aula 3/scripts/scripts.js
+++ aula 3/scripts/scripts.js
@@ -1,8 +1,8 @@
-let square = document.getElementById("Q1")
+let square = document.getElementById("Q1") // id do quadrado
 
 function chanceColor(){
-    let color = document.getElementById('favcolor').value
-    square.style.backgroundColor = color
+    let color = document.getElementById('favcolor').value // pegar a cor
+    square.style.backgroundColor = color // mudar a cor
 }
 
-square.addEventListener('click', chanceColor)
+square.addEventListener('click', chanceColor) // caso ocorra o click, ele mudará de cor
---
+++ aula 4/script.js
@@ -0,0 +1,7 @@
+function adicionar(){
+    add = getElementById("adicionar")
+    item = document.createElement("li")
+    lista = document.getElementsById("Lista")
+
+    lista.appendChild(item)
+};
