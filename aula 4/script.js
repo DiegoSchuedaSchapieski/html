a5e8d3c59c720f756d651cf2e34fbfde4f5b0602
unknown <schuedaschapieski@gmail.com>
2026-02-12 11:35:18 -0300

    complemento

--- aula 4/script.js
+++ aula 4/script.js
@@ -1,7 +1,23 @@
-function adicionar(){
-    add = getElementById("adicionar")
-    item = document.createElement("li")
-    lista = document.getElementsById("Lista")
 
+function adicionar(){
+    let add = document.getElementById("adicionar") // chama o conteudo
+    let item = document.createElement("li") // chama a lista
+    let lista = document.getElementById("lista")
+    let button = document.createElement('button')
+    item.innerHTML = add.value
     lista.appendChild(item)
-};
+    button.textContent = "Deletar"
+    item.appendChild(button)
+
+    
+    button.addEventListener('click', () =>{
+    button.parentElement.remove()
+})
+}
+document.addEventListener("keypress", (e) => {
+    if(e.key == "Enter"){
+        adicionar()
+    }
+})
+    dispatchEvent.append(button)
+
